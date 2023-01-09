abd7aa3df1efd3f3c9009d9ad4468ebd627a302b
Roland Ibbett <rni@Rolands-MacBook-Pro.local>
2023-01-09 20:49:43 +0000

    added source for dasclus

---
+++ global_fns.h
@@ -0,0 +1,15 @@
+
+// Project:	Dashcluster V2
+// Entity	Global Functions
+// File		globals_fns.h
+// Date:	Feb 2006
+
+class mips;
+class p_cache;
+class s_cache;
+class Memory;
+class bus_add;
+class bus_arb;
+class bus_data;
+
+
